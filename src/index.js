f1708a4a6a2709083d8db96f79d399a47ace1bfc
Christian Florez <florez.christian@gmail.com>
2017-07-13 17:01:57 -0400

    add webpack configuration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
-import registerServiceWorker from "./registerServiceWorker";
-import "./index.css";
+import App from "./javascript/components/App";
+import "./stylesheets/index.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
--- src/javascript/components/App.js
+++ src/javascript/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import FilterableGallery from "./FilterableGallery";
-import "../App.css";
 
 class App extends Component {
   render() {

94f0198a61a1cedb475ca94fcfce1b04853c773c
Christian Florez <florez.christian@gmail.com>
2017-07-13 17:27:35 -0400

    add paper card component to photos

--- src/javascript/components/Photo.js
+++ src/javascript/components/Photo.js
@@ -2,6 +2,7 @@ import React from "react";
 import PhotoDetail from "./PhotoDetail";
 import SortableDetail from "./SortableDetail";
 import { dateParser, tagParser, parseCaption } from "../helpers";
+import Paper from 'react-md/lib/Papers';
 
 const Photo = props => {
   const {
@@ -23,21 +24,23 @@ const Photo = props => {
   const parsedCaption = parseCaption(caption);
   return (
     <div className="col-md-4">
-      <div className="col-md-12 well photos">
-        <a href={photoUrl}>
-          <img className="img-responsive" src={src} alt="" />
-        </a>
-        <PhotoDetail label="Posted By" data={<a href={userUrl}>{user}</a>} />
-        <PhotoDetail label="Date" data={parsedDate} />
-        <SortableDetail label="Likes" data={likes} onClick={onSortableClick} />
-        <SortableDetail
-          label="Comments"
-          data={comments}
-          onClick={onSortableClick}
-        />
-        <PhotoDetail label="Filter" data={filter} />
-        <PhotoDetail label="Tags" data={parsedTags} />
-        <PhotoDetail label="Caption" data={parsedCaption} />
+      <div className="col-md-12 photos">
+        <Paper>
+          <a href={photoUrl}>
+            <img className="img-responsive" src={src} alt="" />
+          </a>
+          <PhotoDetail label="Posted By" data={<a href={userUrl}>{user}</a>} />
+          <PhotoDetail label="Date" data={parsedDate} />
+          <SortableDetail label="Likes" data={likes} onClick={onSortableClick} />
+          <SortableDetail
+            label="Comments"
+            data={comments}
+            onClick={onSortableClick}
+          />
+          <PhotoDetail label="Filter" data={filter} />
+          <PhotoDetail label="Tags" data={parsedTags} />
+          <PhotoDetail label="Caption" data={parsedCaption} />
+        </Paper>
       </div>
     </div>
   );

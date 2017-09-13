e5873c4e066c376d87b234d6bc3ea1441ea28b55
Eric Glover <etgt28@mail.missouri.edu>
2017-09-13 13:01:18 -0500

    10^4 credit card icons in a drawer

--- demo_react_tools_forms-master/src/index.js
+++ demo_react_tools_forms-master/src/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './javascripts/components/App';
-import './stylesheets/index.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./javascripts/components/App";
+import "./stylesheets/index.scss";
+
+//material-ui
+import darkBaseTheme from "material-ui/styles/baseThemes/darkBaseTheme";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import AppBar from "material-ui/AppBar";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById("root")
 );
